7e98601a46ac92e66ba02c3c88d67c1b21720e23
fannyting <fannyting0227@gmail.com>
2014-03-20 19:55:48 +0800

    add int push

---
+++ Push/src/com/example/push/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.push;
+
+import com.parse.Parse;
+import com.parse.PushService;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	private EditText editText; 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		Parse.initialize(this, "nlIQe4IIlQBDWDgk3k64XyFsQEzI2EYnuOXtsDr1", "f5Wa8GRUoApMlfKVE9KxjrjuCvjYc2CJg6X0MXoP");
+		PushService.setDefaultPushCallback(this, MainActivity.class);
+		
+		setContentView(R.layout.activity_main);
+		
+		editText = (EditText) findViewById(R.id.editText1);
+	
+	}
+
+	public void click(View view) {
+		String text = editText.getText().toString();
+		Log.d("debug", text);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
